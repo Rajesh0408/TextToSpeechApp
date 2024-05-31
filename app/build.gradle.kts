5aa80853eb99a6d6a1b3be196cd65059d78c5a08
unknown <rajesh.a0408@gmail.com>
2024-05-31 14:50:26 +0530

    final commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.texttospeech"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.texttospeech"
         minSdk = 24
-        targetSdk = 33
+
         versionCode = 1
         versionName = "1.0"
 
--- app/src/main/java/com/example/texttospeech/MainActivity.kt
+++ app/src/main/java/com/example/texttospeech/MainActivity.kt
@@ -1,11 +1,50 @@
 package com.example.texttospeech
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.speech.tts.TextToSpeech
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
+    lateinit var editText: EditText
+    lateinit var textToSpeech: TextToSpeech
+    lateinit var btn: Button
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        editText = findViewById(R.id.editText)
+        btn = findViewById(R.id.btn)
+
+        textToSpeech = TextToSpeech(this) { status->
+            if(status==TextToSpeech.SUCCESS) {
+                Log.d("TextToSpeech","Initialization successful")
+            } else {
+                Log.d("TextToSpeech","Initialization Failed")
+            }
+        }
+
+        textToSpeech.language = Locale.ENGLISH
+
+        btn.setOnClickListener {
+                textToSpeech.speak(editText.text.toString(), TextToSpeech.QUEUE_ADD,null);
+        }
+
+
     }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        textToSpeech.shutdown()
+    }
+
+
 }
